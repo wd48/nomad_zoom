4a7a0738f77821d964de056ef3061558e85ef62d
kastrio_it <wofyd465@gmail.com>
2022-11-28 23:23:13 +0900

    init setting ok

--- src/server.js
+++ src/server.js
@@ -1,7 +1,16 @@
 import express from "express";
+import path from 'path';
+const __dirname = path.resolve();
 
 const app = express();
 
-console.log("hello");
+app.set("view engine","pug");
+app.set("views", __dirname + "/src/views");
+app.use("/public", express.static(__dirname+"/public"));
+// public 폴더를 사용자에게 제공해줌
+app.get("/", (req,res) => res.render("home"));
+app.get("*", (req, res) => res.redirect("/"));
 
-app.listen(3000);
+
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+app.listen(3000, handleListen);
