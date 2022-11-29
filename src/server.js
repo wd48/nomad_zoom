6ddb9c3a2e47db7ec9db1173824d2bdf78fc69cb
kastrio_it <wofyd465@gmail.com>
2022-11-29 19:27:09 +0900

    WebSocket added

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket, { WebSocketServer } from "ws";
 import express from "express";
 import path from 'path';
 const __dirname = path.resolve();
@@ -13,4 +15,8 @@ app.get("*", (req, res) => res.redirect("/"));
 
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+
+const server = http.createServer(app);
+const wss = new WebSocketServer({server});
+
+server.listen(3000, handleListen);
